bcd37d037e820509cf2c379feb4320040bef2dfc
Renato Santos <renato.santos@modalgr.com.br>
2024-05-06 01:34:35 -0300

    remover botão de login desnecessario na main page

--- src/app/Pages/master-component/master-component.component.ts
+++ src/app/Pages/master-component/master-component.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-frame-page',
   template: '<app-navbar></app-navbar><router-outlet></router-outlet>'
 })
+
+//Component Pai, Todos outros components são renderizados à partir do mesmo.
 export class FramePageComponent {
   title = 'Task Managment';
 }
